98968658034bbff40ce19069c9bdbae91b00eff0
Theo Lee <theolee021@gmail.com>
2023-10-21 16:14:41 -0700

    A

--- my-next-app/components/TreePlantingGame.js
+++ my-next-app/components/TreePlantingGame.js
@@ -13,18 +13,21 @@ function TreePlantingGame() {
   });
 
   const [isPlantingMode, setIsPlantingMode] = useState(false);
+  const [isRemovingMode, setIsRemovingMode] = useState(false);
 
   useEffect(() => {
-    // 점수 상태가 변경될 때마다 localStorage에 업데이트
     localStorage.setItem('quizScore', score);
-    // 나무 상태가 변경될 때마다 localStorage에 업데이트
     localStorage.setItem('trees', JSON.stringify(trees));
   }, [score, trees]);
 
   const handlePlantMode = () => {
-    if (score >= 1) {
-      setIsPlantingMode(true);
-    }
+    setIsRemovingMode(false);
+    setIsPlantingMode(!isPlantingMode);
+  };
+
+  const handleRemoveMode = () => {
+    setIsPlantingMode(false);
+    setIsRemovingMode(!isRemovingMode);
   };
 
   const plantTree = (e) => {
@@ -36,6 +39,22 @@ function TreePlantingGame() {
       setTrees([...trees, { x, y, age: 0 }]);
       setScore(prevScore => prevScore - 1);
       setIsPlantingMode(false);
+    }
+
+    if (isRemovingMode) {
+      const rect = e.target.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      // Find the tree near the click position (within a threshold)
+      const threshold = 20; // Adjust this as needed
+      const treeToRemove = trees.find(tree => Math.abs(tree.x - x) < threshold && Math.abs(tree.y - y) < threshold);
+
+      if (treeToRemove) {
+        setTrees(prevTrees => prevTrees.filter(tree => tree !== treeToRemove));
+        setScore(prevScore => prevScore + 1);
+        setIsRemovingMode(false);
+      }
     }
   };
 
@@ -50,6 +69,7 @@ function TreePlantingGame() {
       <h1>Planting Tree Game</h1>
       <p>Seeds: {score}</p>
       <button onClick={handlePlantMode}>Plant a tree (Cost: 1)</button>
+      <button onClick={handleRemoveMode}>Remove a tree (Get back: 1)</button>
       <div className="forest" onClick={plantTree}>
         {trees.map((tree, index) => (
           <div key={index} className={`tree age-${tree.age}`} style={{ left: `${tree.x}px`, top: `${tree.y}px` }} ></div>
