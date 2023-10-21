2aa2e9d0996950e696080620f0189af34a17851c
Theo Lee <theolee021@gmail.com>
2023-10-21 14:50:08 -0700

    a

--- my-next-app/components/TreePlantingGame.js
+++ my-next-app/components/TreePlantingGame.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 function TreePlantingGame() {
-  const [score, setScore] = useState(10);
+  const [score, setScore] = useState(() => {
+    const savedScore = localStorage.getItem('quizScore');
+    return savedScore ? parseInt(savedScore, 10) : 10;
+  });
+  
   const [trees, setTrees] = useState([]);
   const [isPlantingMode, setIsPlantingMode] = useState(false);
+
+  useEffect(() => {
+    // 점수 상태가 변경될 때마다 localStorage에 업데이트
+    localStorage.setItem('quizScore', score);
+  }, [score]);
 
   const handlePlantMode = () => {
     if (score >= 1) {
@@ -18,7 +28,7 @@ function TreePlantingGame() {
       const y = e.clientY - rect.top;
 
       setTrees([...trees, { x, y, age: 0 }]);
-      setScore(score - 1);
+      setScore(prevScore => prevScore - 1);
       setIsPlantingMode(false);
     }
   };
@@ -27,12 +37,15 @@ function TreePlantingGame() {
     <div>
       <h1>Planting Tree Game</h1>
       <p>Seeds: {score}</p>
-      <button onClick={handlePlantMode}>Plant a tree (Cost: 10)</button>
+      <button onClick={handlePlantMode}>Plant a tree (Cost: 1)</button>
       <div className="forest" onClick={plantTree}>
         {trees.map((tree, index) => (
           <div key={index} className={`tree age-${tree.age}`} style={{ left: `${tree.x}px`, top: `${tree.y}px` }} ></div>
-      ))}
+        ))}
       </div>
+      <Link href="/menu" passHref>
+        <button>나가기</button>
+      </Link>
     </div>
   );
 }
--- my-next-app/pages/menu.js
+++ my-next-app/pages/menu.js
@@ -1,17 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import '../styles/style.css';
 
 function MenuPage() {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const savedScore = localStorage.getItem('quizScore');
+    if (savedScore) {
+      setScore(parseInt(savedScore, 10));
+    }
+  }, []);
+
   return (
     <div className="menu-container">
       <h2>Select a Menu</h2>
-      {/* <Link> 컴포넌트 내의 <a> 태그는 단순히 스타일링을 위한 것입니다. */}
       <Link href="/quiz">
         <button className="menu-btn">Quiz</button>
       </Link>
       <Link href="/game">
         <button className="menu-btn">Tree Planting Map</button>
       </Link>
+      <div className="score-display">Your Current Score: {score}</div>
     </div>
   );
 }
